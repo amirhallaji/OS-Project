75580cfdd65466938bf3edef3254ba6c1b7f3cfd
amirhallaji <a.hallaji.b@gmail.com>
2020-11-20 13:34:36 +0330

    Patient.java created

--- src/Patient.java
+++ src/Patient.java
@@ -1,2 +1,25 @@
 public class Patient {
+    private String name;
+    private int duration;
+
+    public Patient(String name, int duration){
+        this.name = name;
+        this.duration = duration;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setDuration(int duration){
+        this.duration = duration;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
 }
